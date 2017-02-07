feb1973201e9b3284dd999c9763722b172e79293
unknown <anzarsh@mail.ru>
2017-02-07 15:38:13 +0300

    add 1

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php require_once('includes/header_top.php'); ?>
 <?php require_once('includes/tech.php'); ?>
 <?php require_once('includes/welcome.php'); ?>
-	
 <?php require_once('includes/footer.php'); ?>
 
 	
