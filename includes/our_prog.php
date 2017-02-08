7dfb0270b4bab19178d8bf5dbafd28940651506e
Berzeg07 <berezg86@gmail.com>
2017-02-08 11:19:45 +0300

    29.14

--- includes/our_prog.php
+++ includes/our_prog.php
@@ -1,1 +1,22 @@
-dhgdfh
+<section>
+	<div class="container">
+		<h2 class="text-center">Наши программы</h2>
+		<div class="dd-box-sircle">
+			<div class="dd-sircle-img">
+				<img src="img/s1.png" alt="">
+			</div>
+			<div class="dd-sircle-img mleft-40">
+				<img src="img/s2.png" alt="">
+			</div>
+		</div>
+		<div class="dd-box-sircle">
+			<div class="dd-sircle-img mleft-40">
+				<img src="img/s3.png" alt="">
+			</div>
+			<div class="dd-sircle-img mleft-40">
+				<img src="img/s3.png" alt="">
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
