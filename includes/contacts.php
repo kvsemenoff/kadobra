aa081ad487f7f56f89ccc6b29aaf936e0eb6b13e
Berzeg07 <berezg86@gmail.com>
2017-02-10 09:41:43 +0300

    29.14

--- includes/contacts.php
+++ includes/contacts.php
@@ -2,5 +2,7 @@
 	<div class="container">
 		<h2>Контакты</h2>
 	</div>
-	<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=F3kR5mRmzK5ZavdGh6LW0APfPWoBSeyQ&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
+	<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=F3kR5mRmzK5ZavdGh6LW0APfPWoBSeyQ&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=false">
+		
+	</script>
 </section>
