91367d29505c0a83c893a3f314f9275445b964b8
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-02-16 14:40:51 +0600

    fix bugs

--- includes/partners.php
+++ includes/partners.php
@@ -6,15 +6,19 @@
 				<div class="img_logo clearfix">
 					<div class="img_logo_1 img_logo-wrap">
 						<img src="img/Kodabra_logo_1.jpg" alt="">
+						<p>Совместное проведение конкурса Young Digital Makers</p>
 					</div>
 					<div class="img_logo_2 img_logo-wrap">
 						<img src="img/Kodabra_logo_2.jpg" alt="">
+						<p>Поддержка и развитие проекта "Кодабра"</p>
 					</div>
 					<div class="img_logo_3 img_logo-wrap">
 						<img src="img/Kodabra_logo_3.jpg" alt="">
+						<p>Поддержка форума YDM 13.12.2015</p>
 					</div>
 					<div class="img_logo_4 img_logo-wrap">
 						<img src="img/Kodabra_logo_4.jpg" alt="">
+						<p>Организация сети клубов цифрового творчества в Москве и МО</p>
 					</div>
 					<div class="img_logo_5 img_logo-wrap">
 						<img src="img/Kodabra_logo_5.jpg" alt="">
@@ -22,12 +26,6 @@
 					<div class="img_logo_6 img_logo-wrap">
 						<img src="img/Kodabra_logo_6.jpg" alt="">
 					</div>
-				</div>
-				<div class="support">
-					<p>Совместное проведение конкурса Young Digital Makers</p>
-					<p>Поддержка и развитие проекта "Кодабра"</p>
-					<p>Поддержка форума YDM 13.12.2015</p>
-					<p>Организация сети клубов цифрового творчества в Москве и МО</p>
 				</div>
 			</div>
 		</div>
